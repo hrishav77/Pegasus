b20be8a73e381f4674d88983fbc89aeeb46e9c56
R4d104ct1v1ty <101217134+R4d104ct1v1ty@users.noreply.github.com>
2023-03-24 15:18:32 +0530

    added backend

---
+++ src/components/FilterPanel.js
@@ -0,0 +1,10 @@
+function FilterPanel() {
+    return(
+        <div className="filterContainer">
+            <h2>FILTER</h2>
+            <div className="searchBars">
+                <div className="row"></div>
+            </div>
+        </div>
+    )
+}
