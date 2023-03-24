64bcaed2d99b61df7cda04fbb13526a375cd94d2
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-03-24 17:15:58 +0530

    star button looks

--- src/components/Doubtbox.js
+++ src/components/Doubtbox.js
@@ -1,5 +1,6 @@
 import { Flex, Spacer, Text,Button} from '@chakra-ui/react'
 import React from 'react'
+import { useState } from 'react';
 
 
 import { Link } from 'react-router-dom';
@@ -10,7 +11,12 @@ import Box from './Box'
 let curr_dt = new Date()
 let date = curr_dt.getDay() + "/" + (curr_dt.getMonth() + 1) + "/" + curr_dt.getFullYear() + " " + curr_dt.getHours() + ":" + curr_dt.getMinutes() ;
 export default function Doubtbox(props) {
-
+const [isStarred,SetStarred]=useState(false)
+const starredButton=()=>{
+  SetStarred((isStarred)=>{
+    return !isStarred;
+  })
+}
   
   return (
     <Box w="100%" h="fit-content">
@@ -21,7 +27,7 @@ export default function Doubtbox(props) {
         </Flex>  
     <Text fontSize="xl" fontWeight="600">{props.title}</Text>
     <Text>{props.body}</Text>
-    <Button borderRadius="10px" variant="outline"><i class="fi fi-rr-star"></i></Button>
+    <Button borderRadius="10px" variant="outline" ><i class="fi fi-rr-star" style={{backgroundColor:isStarred?"goldenrod":""}} onClick={starredButton}></i></Button>
     <Button borderRadius="10px" variant="outline" ml="10px">
       <Link to="/solutionpage">Solutions</Link>
       </Button>
