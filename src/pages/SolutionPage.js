3887444c63baf91c2345de7f010956921a0fc03d
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-03-25 09:53:36 +0530

    left bar
    added left bar in solution page

--- src/pages/SolutionPage.js
+++ src/pages/SolutionPage.js
@@ -1,16 +1,21 @@
+import AccountPanel from "../components/AccountPanel";
 import Navbar from "../components/Navbar"
-import styles from "./SolutionPage.module.css";
+import RoomPanel from "../components/RoomPanel";
+import styles from "./DoubtPage.module.css";
 
 const settings = require("../settings");
+const rooms=settings.rooms;
+const username="var user"
 
 const SolutionPage = () => {
     return ( <div className="solutionpage">
         <div className={styles.content}>
             <Navbar />
             <div className={styles.doubtpagecontent}>
-                <div className={styles.leftsidebar}>
-                    THIS IHIUefhiusdhfiuhwaoiufh
-                </div>
+                    <div className={styles.leftsidebar}>
+                        <AccountPanel username={username}/>
+                        <RoomPanel rooms={rooms}/>
+                    </div>
                 <div className={styles.rightsidebar}>
                     mwdouhiuhphufeppefwqp
                 </div>
