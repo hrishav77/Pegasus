f5351290bf84fa6a9a6da475a2cc672626af0f04
AmalNambiar12 <f20212670@goa.bits-pilani.ac.in>
2023-03-25 09:50:08 +0530

    Made changes to HelpBox

--- src/components/HelpBox.js
+++ src/components/HelpBox.js
@@ -1,11 +1,17 @@
+import styles from "./HelpBox.module.css";
+
 const settings = require("../settings");
 
 export default function HelpBox(){
     const appName = settings.appName;
     return (
-        <div className="helpBox">
+        <div className={styles.helpbox}>
             <h2 className="help">HELP</h2>
-            <p>Welcome to your {appName} Account Dashboard</p>
+            <p>Welcome to your {appName} Account Dashboard!</p><br />
+            <p>Here, you can view doubts personally handpicked by you, for you. Use 
+                this Dashboard to effectively view the solutions to doubts that puzzle you.</p><br />
+            <p>If you wish to continue solving doubts on the forum, pick one of the Subject Rooms
+                on the left side panel and proceed there to continue solving.</p><br />
         </div>
     );
 }
