cfac61c559352104e7625f49f9d4890868e4ed70
AmalNambiar12 <f20212670@goa.bits-pilani.ac.in>
2023-03-25 12:04:26 +0530

    Update SolutionPage.js

--- src/pages/SolutionPage.js
+++ src/pages/SolutionPage.js
@@ -1,23 +1,31 @@
 import { Flex, Spacer } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import AccountPanel from "../components/AccountPanel";
 import Navbar from "../components/Navbar"
 import Overlay from "../components/OverlayPost";
 import OverlaySol from"../components/OverlaySol"
 import RoomPanel from "../components/RoomPanel";
 import styles from "./DoubtPage.module.css";
-
 const settings = require("../settings");
-const rooms=settings.rooms;
-const username="var user"
+
+
 
 const SolutionPage = () => {
+    const nav = useNavigate();
+    const rooms=settings.rooms;
+    const username="var user"
+
+    const roomSwitch = (x) => {
+        nav("/doubtpage/?room="+x);
+    }
+
     return ( <div className="solutionpage">
         <div className={styles.content}>
             <Navbar />
             <div className={styles.doubtpagecontent}>
                     <div className={styles.leftsidebar}>
                         <AccountPanel username={username}/>
-                        <RoomPanel rooms={rooms}/>
+                        <RoomPanel rooms={rooms} onSwitchRoom={roomSwitch}/>
                     </div>
                 
                 <div className={styles.rightsidebarsol}>
