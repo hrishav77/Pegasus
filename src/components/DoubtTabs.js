cac70ec0b51fc7a9fc29dc820063ed172e920423
R4d104ct1v1ty <101217134+R4d104ct1v1ty@users.noreply.github.com>
2023-03-25 09:09:51 +0530

    added tabs in account Page

--- src/components/DoubtTabs.js
+++ src/components/DoubtTabs.js
@@ -3,19 +3,19 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 export default function DoubtTabs() {
     return (
     <div className="tabs">
-        <Tabs size='md' variant='enclosed'>
-    <TabList>
-        <Tab>Your Doubts</Tab>
-        <Tab>Starred Doubts</Tab>
-    </TabList>
-    <TabPanels>
-        <TabPanel>
-        <p>one!</p>
-        </TabPanel>
-        <TabPanel>
-        <p>two!</p>
-        </TabPanel>
-    </TabPanels>
-    </Tabs>
+        <Tabs isFitted variant='enclosed'>
+  <TabList mb='1em' bg='rgba(255, 255, 255, 0.8)'>
+    <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Your Doubts</Tab>
+    <Tab _selected={{ color: 'white', bg: 'blue.500' }}>Starred Doubts</Tab>
+  </TabList>
+  <TabPanels>
+    <TabPanel>
+      <p>one!</p>
+    </TabPanel>
+    <TabPanel>
+      <p>two!</p>
+    </TabPanel>
+  </TabPanels>
+</Tabs>
     </div>);    
 }
